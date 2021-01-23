8c1bc018f352d11d2665abdbc6974cf3d32c8ba3
zsf90 <freerealmshn@163.com>
2021-01-23 15:03:24 +0800

    添加 Makefile 添加 cmake_base 项目

---
+++ cmake_demo/cmake_base/src/main.c
@@ -0,0 +1,23 @@
+/*
+ * @Author: 信念D力量
+ * @Github: https://www.github.com/fy2008
+ * @Gitee: https://gitee.com/zsf90
+ * @website: https://fy2008.github.io
+ * @FilePath: /CExample/cmake_demo/cmake_base/src/main.c
+ * @Date: 2021-01-23 15:05:45
+ * @LastEditTime: 2021-01-23 15:08:00
+ * @LastEditors: Please set LastEditors
+ * @Copyright(C): 信念D力量 (freerealmshn@163.com)
+ * All Rights Reserved.
+ * ----------------------------------------------
+ * @Description: CMake 基础知识练习
+ * ----------------------------------------------
+ * 
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("hello,world!\n");
+    return 0;
+}
